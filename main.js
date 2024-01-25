c8961f25024906a1d8fc814aeb8bee574a4c4859
shinhosuck <shinhosuck1973@gmail.com>
2024-01-25 14:45:22 +0800

    update

--- main.js
+++ main.js
@@ -3,12 +3,14 @@ const colorOverlay = document.querySelector('.color-overlay')
 const links = document.querySelector('.navbar-container__links')
 const openNavLinksBtn = document.querySelector('.navbar-container__open-links-btn')
 const closeNavLinksBtn = document.querySelector('.navbar-container__close-links-btn')
+const navLinkAnchor = [...document.querySelectorAll('.navbar-container__anchor-tag')]
 
 const handleEvents = (e)=> {
     if(e.currentTarget === openNavLinksBtn) {
         links.classList.add('show-navlinks')
         document.body.style.overflowY = 'hidden'
         colorOverlay.style.display = 'block'
+        window.scrollTo({top:0})
     }else if(e.currentTarget === closeNavLinksBtn) {
         links.classList.remove('show-navlinks')
         document.body.style.overflowY = 'scroll'
@@ -18,3 +20,20 @@ const handleEvents = (e)=> {
 
 openNavLinksBtn.addEventListener('click', handleEvents)
 closeNavLinksBtn.addEventListener('click', handleEvents)
+
+const getPath = ()=> {
+    const path = window.location.pathname
+    const name = path.split('/').filter((i)=>i !== '').slice(-1).join('').replaceAll('.html', '')
+    const id = document.querySelector(`#${name}`)
+    const href = id.href.split('/').filter((i)=>i !== '').slice(-1).join('').replaceAll('.html', '') === name
+    if(href) {
+        id.classList.add('active-navlink')
+    }
+}
+
+getPath()
+// window.addEventListener('resize', ()=> {
+//     const heroContainerHeader = document.querySelector('.hero-container__header')
+//     const fontSize = window.getComputedStyle(heroContainerHeader).fontSize
+//     console.log(fontSize)
+// })
